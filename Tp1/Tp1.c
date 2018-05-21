a33f893ab45a37472bb1df1d0867a2c49b03d85c
chiarama <37699529+chiarama@users.noreply.github.com>
2018-05-21 20:05:14 -0300

    Corregido

--- Tp1/Tp1.c
+++ Tp1/Tp1.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <conio.h>
 
-
 int clasificarChar (char unChar){
 	int a = 5;
 	if (unChar == '0'){a=0;};
@@ -15,7 +14,6 @@ int clasificarChar (char unChar){
 	return a;
 };
 int main()
-         //int argc, char const *argv[])
 {
 	int productions[7][7] = {
 		{2,1,1,6,6,6},
@@ -26,18 +24,14 @@ int main()
 		{5,5,5,5,6,6},
 		{6,6,6,6,6,6}
 	};
-
 	int currentStatus = 0;
 	char* currentWord = "";
-
 	FILE *archivoDeEntrada = fopen( "archivoDeEntrada.txt", "r" );
 	FILE *archivoDeSalida = fopen( "archivoDeSalida.txt", "w" );
-
-	while(!feof(archivoDeEntrada)){ //Entra en bucle infinito (no termina al menos que se depure, y escribe repetidas veces “0x” en el archivo.
-		char nextChar; //proxima letra del archivo, DECLARARLO
-		//algo con fread sizeof(char)*1,1, SEEK_CUR
+	while(!feof(archivoDeEntrada)){ 
+		char nextChar; 
 		fread(&nextChar, 1, 1, archivoDeEntrada);
-		while (nextChar != 32){
+		while ((nextChar != 32) && (!feof(archivoDeEntrada))){
             		currentStatus = productions [currentStatus][clasificarChar(nextChar)];
             		fwrite(&nextChar,1, 1, archivoDeSalida);
 			fread(&nextChar, 1, 1, archivoDeEntrada);
