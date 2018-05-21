6295d086845194fa7a4b57e6c471fdf4bfcd2bd8
Juan Martín Gordo <juanma.gordo99@gmail.com>
2018-05-21 19:22:45 -0300

    Falta corregir el error de EOF asi el programa termina y cierra los archivos, lo demas funciona todo.

---
+++ Tp1/Tp1.c
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <conio.h>
+
+/*
+//el objetivo es clasificar el tipo de constante entera que es
+//mediante el uso del automata de c
+int clasificarChar (char unChar){
+	int a = 5;
+	if (unChar == '0'){a=0;};
+	if (49<=unChar<=55) {a=1;};
+	if (unChar == 'a' || unChar == 'b'|| unChar == 'c'|| unChar == 'd'|| unChar == 'e'|| unChar == 'f'){a=3;};
+	if (unChar == 'A' || unChar == 'B'|| unChar == 'C'|| unChar == 'D'|| unChar == 'E'|| unChar == 'F'){a=3;};
+	if (unChar == 'X' || unChar == 'x'){a=4;};
+	if (unChar == '8' || unChar == '9'){a=2;};
+	return a;
+
+};
+
+int main()
+         //int argc, char const *argv[])
+{
+	int productions[7][7] = {
+		{2,1,1,6,6,6},
+		{1,1,1,6,6,6},
+		{3,3,6,6,4,6},
+		{3,3,6,6,6,6},
+		{5,5,5,5,6,6},
+		{5,5,5,5,6,6},
+		{6,6,6,6,6,6}
+	};
+
+	//int finalStatus[?] = {};
+	enum typeOfEntry {  space,                    //' '
+						//zero,                     //0
+						octalEntry,               //1-7
+						decimalEntry,             //8,9
+						hexEntry,                 //A-F, a-f
+						x,                        //X,x
+						err                       //other
+	} ;
+
+
+	int currentStatus = 0;
+	char* currentWord = "";
+
+	FILE *archivoDeEntrada = fopen( "archivoDeEntrada.txt", "r" );
+	FILE *archivoDeSalida = fopen( "archivoDeSalida.txt", "w" );
+	while(!feof(archivoDeEntrada)){   // Mientras no termine el archivo de entrada
+		char nextChar; //proxima letra del archivo, DECLARARLO
+		//algo con fread sizeof(char)*1,1, SEEK_CUR
+
+		fread(&nextChar, 2, 1, archivoDeEntrada);
+		while (nextChar != ' '){
+            		currentStatus = productions [currentStatus][clasificarChar(nextChar)];
+            		fwrite(&nextChar,2, 1, archivoDeSalida);
+			fread(&nextChar,2, 1, archivoDeEntrada);
+		};
+        	printf ("Read/write");
+
+		if (currentStatus != 0){
+                             	switch (currentStatus) {
+				case 1:
+					currentWord = "    Decimal";
+				break;
+				case 5:
+					currentWord = "    Hexadecimal";
+				break;
+				case 2:
+				case 3:
+					currentWord = "    Octal";
+				break;
+				case 4:
+				case 6:
+					currentWord = "    No es una consante entera";
+				break;
+			};
+
+		       	fwrite(currentWord ,2, strlen(currentWord), archivoDeSalida);
+		};
+
+		currentStatus = 0;
+	};
+		printf ("Case y fin");
+
+
+	fclose (archivoDeEntrada);
+	fclose (archivoDeSalida);
+    printf ("Archivo creado.");
+
+	return 0;
+};
+*/
+
+int clasificarChar (char unChar){
+	int a = 5;
+	if (unChar == '0'){a=0;};
+	if (unChar == '1' || unChar == '2'|| unChar == '3'|| unChar == '4'|| unChar == '5'|| unChar == '6'|| unChar == '7'){a=1;};
+	if (unChar == 'a' || unChar == 'b'|| unChar == 'c'|| unChar == 'd'|| unChar == 'e'|| unChar == 'f'){a=3;};
+	if (unChar == 'A' || unChar == 'B'|| unChar == 'C'|| unChar == 'D'|| unChar == 'E'|| unChar == 'F'){a=3;};
+	if (unChar == 'X' || unChar == 'x'){a=4;};
+	if (unChar == '8' || unChar == '9'){a=2;};
+	return a;
+};
+int main()
+         //int argc, char const *argv[])
+{
+	int productions[7][7] = {
+		{2,1,1,6,6,6},
+		{1,1,1,6,6,6},
+		{3,3,6,6,4,6},
+		{3,3,6,6,6,6},
+		{5,5,5,5,6,6},
+		{5,5,5,5,6,6},
+		{6,6,6,6,6,6}
+	};
+
+	int currentStatus = 0;
+	char* currentWord = "";
+
+	FILE *archivoDeEntrada = fopen( "archivoDeEntrada.txt", "r" );
+	FILE *archivoDeSalida = fopen( "archivoDeSalida.txt", "w" );
+
+	while(!feof(archivoDeEntrada)){ //Entra en bucle infinito (no termina al menos que se depure, y escribe repetidas veces “0x” en el archivo.
+		char nextChar; //proxima letra del archivo, DECLARARLO
+		//algo con fread sizeof(char)*1,1, SEEK_CUR
+		fread(&nextChar, 1, 1, archivoDeEntrada);
+		while (nextChar != 32){
+            		currentStatus = productions [currentStatus][clasificarChar(nextChar)];
+            		fwrite(&nextChar,1, 1, archivoDeSalida);
+			fread(&nextChar, 1, 1, archivoDeEntrada);
+		};
+		if (currentStatus != 0){
+                             	switch (currentStatus) {
+				case 1:
+					currentWord = "    Decimal\n";
+				break;
+				case 5:
+					currentWord = "    Hexadecimal\n";
+				break;
+				case 2:
+				case 3:
+					currentWord = "    Octal\n";
+				break;
+				case 4:
+				case 6:
+					currentWord = "    No es una constante entera\n";
+				break;
+			};
+		       	fwrite(currentWord ,1, strlen(currentWord), archivoDeSalida);
+		};
+		currentStatus = 0;
+	};
+	fclose (archivoDeEntrada);
+	fclose (archivoDeSalida);
+	printf ("Archivo creado.");
+	return 0;
+};
+
+
+
+
+
+
